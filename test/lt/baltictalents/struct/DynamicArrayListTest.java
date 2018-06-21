8ca92d249aafe427ade02ce28095ea00bfe35ae7
gsmoffln <gsmoffln@gmail.com>
2018-06-21 20:16:50 +0300

    Added Jupiter (JUnit 5 tests+libs).

---
+++ test/lt/baltictalents/struct/DynamicArrayListTest.java
@@ -0,0 +1,132 @@
+package lt.baltictalents.struct;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DynamicArrayListTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void ensureSize() {
+
+  }
+
+  @Test
+  void add() {
+
+  }
+
+  @Test
+  void remove() {
+  }
+
+  @Test
+  void remove1() {
+  }
+
+  @Test
+  void contains() {
+  }
+
+  @Test
+  void size() {
+  }
+
+  @Test
+  void _clone() {
+  }
+
+  @Test
+  void _toString() {
+  }
+
+  @Test
+  void checkVersion() {
+  }
+
+  @Test
+  void ensureSize1() {
+  }
+
+  @Test
+  void ensureInRange() {
+  }
+
+  @Test
+  void add1() {
+  }
+
+  @Test
+  void remove2() {
+  }
+
+  @Test
+  void add2() {
+  }
+
+  @Test
+  void remove3() {
+  }
+
+  @Test
+  void set() {
+  }
+
+  @Test
+  void add3() {
+  }
+
+  @Test
+  void get() {
+  }
+
+  @Test
+  void iterator() {
+  }
+
+  @Test
+  void sort() {
+  }
+
+  @Test
+  void main() {
+  }
+
+  @Test
+  void contains1() {
+  }
+
+  @Test
+  void size1() {
+  }
+
+  @Test
+  void clone1() {
+  }
+
+  @Test
+  void equals() {
+  }
+
+  @Test
+  void _hashCode() {
+  }
+
+  @Test
+  void items() {
+  }
+
+  @Test
+  void compareTo() {
+  }
+}
