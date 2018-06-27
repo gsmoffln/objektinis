5b92d0751f58f5f365232ae780e734f4cf8be257
gsm <gsmoffln@gmail.com>
2018-06-27 22:45:21 +0300

    Ček reindž

--- src/lt/baltictalents/stat/UnitedStatesCensusGenderSelector.java
+++ src/lt/baltictalents/stat/UnitedStatesCensusGenderSelector.java
@@ -6,7 +6,7 @@ public class UnitedStatesCensusGenderSelector implements DistributedFunction<Gen
 
     @Override
     public Gender select(double v) {
-
+        assureRange0to1(v);
 
         // pagal cenzą, parenkam MALE arba FEMALE
         if((int)(v*totalPopulation) < malePopulation) {
@@ -20,6 +20,7 @@ public class UnitedStatesCensusGenderSelector implements DistributedFunction<Gen
     public static void main(String[] args) {
         DistributedFunction<Gender> gender = new UnitedStatesCensusGenderSelector();
         System.out.println(gender.select(0.1));
+        System.out.println(gender.select(0.48));
         System.out.println(gender.select(0.5));
         System.out.println(gender.select(0.9));
         System.out.println(gender.select(100.0));
